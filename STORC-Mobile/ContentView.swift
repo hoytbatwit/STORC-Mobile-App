f42e8a150da22de293660340d442dd3db7dc8e2e
Brian Hoyt <hoytb@wit.edu>
2023-06-07 21:06:03 -0400

    Initial Commit

---
+++ STORC-Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  STORC-Mobile
+//
+//  Created by Brian H on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
