fa6e8b07605e90bf72ad6383a3d8f27c853273f6
Brian Hoyt <hoytb@wit.edu>
2023-06-15 17:51:31 -0400

    working on getting HR data

--- STORC-Mobile-App/ViewController.swift
+++ STORC-Mobile-App/ViewController.swift
@@ -6,21 +6,45 @@
 //
 
 import UIKit
-import SwiftUI
+import HealthKit
 
 class ViewController: UIViewController {
 
     //Working on timer stuff for manual contraction
-    @State var timerRunning = false
-    @State var ellapsedTime = 0
+    var timerRunning = false
+    var ellapsedTime = 0
     var buttonState = 0
+    @IBOutlet weak var heartRate: UILabel!
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var healthStore : HKHealthStore?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func getHealth(){
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+        }else{
+            //no health data available not sure how to handle this
+            //throw error?
+        }
+        
+        //Request write access to Heart Rate Data
+        let dataType = HKQuantityType.quantityType(forIdentifier: .heartRate)
+        healthStore?.requestAuthorization(toShare: [dataType], read: nil) { success, error in
+            if success {
+                //save sample or call method
+            }else{
+                //Failed request not denied authorization can retry
+            }
+        }
+    }
+    
+    
     @IBAction func manualButton(_ sender: UIButton) {
         switch buttonState{
         case 1:
@@ -31,12 +55,10 @@ class ViewController: UIViewController {
             buttonState = 1
         default:
             sender.setTitle("Tap here when a contraction begins", for: .normal)
-        
-        
         }
-        
-        
-        timerRunning = true
+        if(buttonState == 1){
+            
+        }
     }
 }
 
