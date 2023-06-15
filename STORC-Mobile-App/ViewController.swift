d78dff1beda848f7e2420e3160cd4ff0c49028f9
Brian Hoyt <hoytb@wit.edu>
2023-06-15 16:58:29 -0400

    got button working the way I want it to

--- STORC-Mobile-App/ViewController.swift
+++ STORC-Mobile-App/ViewController.swift
@@ -6,16 +6,37 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
+    //Working on timer stuff for manual contraction
+    @State var timerRunning = false
+    @State var ellapsedTime = 0
+    var buttonState = 0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func manualButton(_ sender: UIButton) {
-        sender.setTitle("Tap here when the contraction ends", for: .normal)
+        switch buttonState{
+        case 1:
+            sender.setTitle("Tap here when the contraction ends", for: .normal)
+            buttonState = 0
+        case 0:
+            sender.setTitle("Tap here when a contraction begins", for: .normal)
+            buttonState = 1
+        default:
+            sender.setTitle("Tap here when a contraction begins", for: .normal)
+        
+        
+        }
+        
+        
+        timerRunning = true
     }
 }
 
