a26088fc76482f207c1a8fee4707beb63a273b13
Brian Hoyt <hoytb@wit.edu>
2023-06-20 17:07:26 -0400

    now able to query health kit need to figure out how to update UI

--- STORC-Mobile-App/ViewController.swift
+++ STORC-Mobile-App/ViewController.swift
@@ -12,12 +12,12 @@ import SwiftUI
 class ViewController: UIViewController {
 
     //Working on timer stuff for manual contraction
-    var timerRunning = false
-    var ellapsedTime = 0
-    var buttonState = 0
+    //var timerRunning = false
+    //var ellapsedTime = 0
     @IBOutlet weak var heartRate: UILabel!
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    
+    // going to be used for contraction timing not implemented yet
+    //let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    var buttonState = 0
     var healthStore : HKHealthStore?
 
     
@@ -28,61 +28,85 @@ class ViewController: UIViewController {
     
     //first we need to get authorization to use health kit data
     func getHealthAuth(){
+        print("Trying to get health auth")
+        //will ensure that health data is available
         if HKHealthStore.isHealthDataAvailable() {
             healthStore = HKHealthStore()
         }else{
             //no health data available not sure how to handle this
             //throw error?
+            print("There was an error need a way to handle it")
         }
         
         //Request read/write access to Heart Rate Data
         let dataTypes = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!, HKObjectType.quantityType(forIdentifier: .restingHeartRate)!])
-        
         healthStore?.requestAuthorization(toShare: dataTypes, read: dataTypes) { success, error in
+            //We got authorization
             if success{
-                //We got authorization
+                print("Health auth successfull")
                 self.getHeartRateData()
             }else{
                 //there was an error we can retry throw error or ask again?
-                self.getHealthAuth()
+                print("Some error hopefully able to see in debug console.")
             }
         }
     }
     
     //next we actually get the health kit data that we need for the app
     func getHeartRateData(){
+        print("trying to set up heart rate data")
         //type we are querying
-        let heartRate = HKObjectType.quantityType(forIdentifier: .heartRate)!
-        //setting up calendar for date range
-        let calendar = Calendar(identifier: .gregorian)
-        //March 18th 2017 is when sample data was taken
-        var dateComponents = DateComponents()
-        dateComponents.year = 2017
-        dateComponents.month = 3
-        dateComponents.day = 18
-        
-        let startDate = calendar.date(from: dateComponents)
-        let endDate = calendar.date(from: dateComponents)
-        let pred = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
-        let sort = [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)]
-        let query = HKSampleQuery(sampleType: heartRate, predicate: pred, limit: 25, sortDescriptors: sort, resultsHandler: { (query, results,error) in
-            guard error == nil else { print("error"); return }
-        
-            self.printHeartRateInfo(results: results)
-        })
-        
-        healthStore?.execute(query)
-        
-    }
-    
-    func printHeartRateInfo(results:[HKSample]?){
-        let heartRateUnit:HKUnit = HKUnit(from: "count/min")
-        for(_, sample) in results!.enumerated(){
-            guard let currData:HKQuantitySample = sample as? HKQuantitySample else {return}
-            
-            print("\(sample)")
-            print("Heart Rate: \(currData.quantity.doubleValue(for: heartRateUnit))")
+        guard let heartRate = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+            fatalError("This should never fail")
         }
+        
+        // setting up calendar for date range
+        // I have a feeling this will be needed so going to write down the code for getting the current day
+        /*
+        let calendar = NSCalendar.current
+        let new = Date()
+        let componenets = calendar.dateComponents([.year, .month, .day], from: now)
+        guard let startDate = calendar.date(from: components) else {
+            fatalError("*** Unable to create start date")
+        }
+
+        guard let endDate = calendar.date(byAdding: .day, value: 1, to: startDate) else {
+            fatalError("*** Unable to create end date")
+        }
+        let today = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+        let sortByDate = NSSortDescriptor(key: HKSampleSortIdentifiedStartDate, ascending: true)
+        let filterQuery = HKSampleQuery(sampleType: heartRate, predicate: today, limit: Int(HKObjectQueryNoLimit), sortDescriptors: [sortByDate]) {
+            query, results, error in
+        }
+         */
+        
+        // query that is not filtered returns all results
+        let heartRateUnit:HKUnit = HKUnit(from: "count/min")
+        //let timeUnit:HKUnit = HKUnit(from: "min")
+        let query = HKSampleQuery(sampleType: heartRate, predicate: nil, limit: Int(HKObjectQueryNoLimit), sortDescriptors: nil) {
+            query, results, error in
+            
+            guard let samples = results as? [HKQuantitySample] else {
+                //handle errors
+                print("There was an error")
+                return
+            }
+            
+            for sample in samples {
+                //process sample here
+                //print("\(sample)")
+                print("\(sample.startDate)")
+                //print("Heart Rate: \(sample.quantity.doubleValue(for: heartRateUnit))")
+                //print("Heart Rate: \(sample.quantity.doubleValue(for: timeUnit))")
+            }
+            
+            //results come back on an anonymous background queue
+            //dispatch to the main queue before modifying UI
+            DispatchQueue.main.async {
+                //Update UI here
+            }
+        }
+        healthStore?.execute(query)
     }
     
     //update UI with the health data that we got from the query
@@ -122,15 +146,16 @@ class ViewController: UIViewController {
         case 1:
             sender.setTitle("Tap here when the contraction ends", for: .normal)
             self.getHealthAuth()
+            print("state is contraction is happening")
             buttonState = 0
         case 0:
             sender.setTitle("Tap here when a contraction begins", for: .normal)
+            print("state is no contraction")
             buttonState = 1
         default:
-            sender.setTitle("Tap here when a contraction begins", for: .normal)
-        }
-        if(buttonState == 1){
-            
+            //sender.setTitle("Tap here when a contraction begins", for: .normal)
+            //buttonState = 1
+            return
         }
     }
     
